45b155bfb453cfc51738bbfd16457bef01c650c1
FranMontesSegales <95092587+FranMontesSegales@users.noreply.github.com>
2022-10-04 21:01:49 +0200

    ejercicio1

---
+++ DWES/1Trimestre/ejercicio1.js
@@ -0,0 +1,17 @@
+
+
+function primo(numero = 100) {
+
+    for (let i = 0; i < numero; i++) {
+        if(i%3  === 0 && i%5  === 0){
+            console.log(i + " " + "fizzbuzz")
+        } else if (i%3  === 0) {
+        console.log(i + " " + "fizz")
+      }else if (i%5  === 0) {
+        console.log(i + " " + "buzz")
+      }else{
+        console.log(i)
+      }
+    }
+  }
+  primo();
